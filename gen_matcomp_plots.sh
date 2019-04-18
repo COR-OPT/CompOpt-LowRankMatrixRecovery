80c2f8491bfa340ad82cfa14cbb9b72d68026d8f
VHarisop <vharisop@gmail.com>
2019-04-17 20:33:10 -0400

    Removed unpolished scripts

--- gen_matcomp_plots.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python matcomp_phase.py --noheader --i matcomp_subgrad.csv \
-	--out_file matcomp_subgrad.svg
-inkscape -D -z --file=matcomp_subgrad.svg \
-	--export-pdf=matcomp_subgrad.pdf --export-latex
