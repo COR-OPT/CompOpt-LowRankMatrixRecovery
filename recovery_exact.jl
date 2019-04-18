6039071209106d07ec9fc06acf518cf82927799b
VHarisop <vharisop@gmail.com>
2019-04-17 20:31:09 -0400

    Added recovery experiments with plotting

---
+++ recovery_exact.jl
@@ -0,0 +1,207 @@
+using ArgParse
+using LinearAlgebra
+using Printf
+using PyPlot
+using Random
+using Statistics
+
+include("src/CompOpt.jl")
+
+LBLUE = "#908cc0"
+MBLUE = "#519cc8"
+HBLUE = "#1d5996"
+LRED = "#cb5501"
+MRED = "#f1885b"
+HRED = "#b3001e"
+
+zero_pad!(v, lenTotal) = append!(v, zeros(lenTotal - length(v)))
+
+#= bilinear sensing =#
+function bilin_experiment(d1, d2, r, iters, delta; algo_type=:subgradient)
+	r = 5; m = 8 * r * (d1 + d2)
+	prob_mild = CompOpt.genBilinProb(d1, d2, m, r, 0.25)
+	prob_high = CompOpt.genBilinProb(d1, d2, m, r, 0.40)
+	ds_mild = nothing; ds_high = nothing
+	style = (algo_type == :subgradient) ? "-" : ".-"  # line style
+	println("Running for rank $(r)...")
+	if algo_type == :subgradient
+		_, _, ds_mild = CompOpt.pSgd_init(prob_mild, iters, delta, eps=1e-12)
+		_, _, ds_high = CompOpt.pSgd_init(prob_high, iters, delta, eps=1e-12)
+	else
+		_, _, ds_mild = CompOpt.proxlin_init(prob_mild, delta, iters, eps=1e-12)
+		_, _, ds_high = CompOpt.proxlin_init(prob_high, delta, iters, eps=1e-12)
+	end
+	semilogy(collect(1:length(ds_mild)), ds_mild, color=LBLUE, style,
+	         label=L"$ (r, p) = (5, 0.25) $")
+	semilogy(collect(1:length(ds_high)), ds_high, color=MBLUE, style,
+	         label=L"$ (r, p) = (5, 0.40) $")
+	r = 10; m = 8 * r * (d1 + d2)
+	prob_mild = CompOpt.genBilinProb(d1, d2, m, r, 0.25)
+	prob_high = CompOpt.genBilinProb(d1, d2, m, r, 0.40)
+	println("Running for rank $(r)...")
+	if algo_type == :subgradient
+		_, _, ds_mild = CompOpt.pSgd_init(prob_mild, iters, delta, eps=1e-12)
+		_, _, ds_high = CompOpt.pSgd_init(prob_high, iters, delta, eps=1e-12)
+	else
+		_, _, ds_mild = CompOpt.proxlin_init(prob_mild, delta, iters, eps=1e-12)
+		_, _, ds_high = CompOpt.proxlin_init(prob_high, delta, iters, eps=1e-12)
+	end
+	semilogy(collect(1:length(ds_mild)), ds_mild, color=HBLUE, style,
+	         label=L"$ (r, p) = (10, 0.25) $")
+	semilogy(collect(1:length(ds_high)), ds_high, color="black", style,
+	         label=L"$ (r, p) = (10, 0.40) $")
+	xlabel(L"$ k $"); ylabel("Normalized error")
+	title("Bilinear sensing - $(algo_type) method"); legend(); show()
+end
+
+
+#= symmetrized quadratic sensing =#
+function quad_experiment(d, r, iters, delta; algo_type=:subgradient)
+	r = 5; m = 8 * r * d
+	prob_mild = CompOpt.genSymQuadProb(d, m, r, 0.25)
+	prob_high = CompOpt.genSymQuadProb(d, m, r, 0.40)
+	ds_mild = nothing; ds_high = nothing
+	style = (algo_type == :subgradient) ? "-" : ".-"  # line style
+	println("Running for rank $(r)...")
+	if algo_type == :subgradient
+		_, ds_mild = CompOpt.pSgd_init(prob_mild, iters, delta, eps=1e-12)
+		_, ds_high = CompOpt.pSgd_init(prob_high, iters, delta, eps=1e-12)
+	else
+		_, ds_mild = CompOpt.proxlin_init(prob_mild, delta, iters, eps=1e-12)
+		_, ds_high = CompOpt.proxlin_init(prob_high, delta, iters, eps=1e-12)
+	end
+	semilogy(collect(1:length(ds_mild)), ds_mild, color=LBLUE, style,
+	         label=L"$ (r, p) = (5, 0.25) $")
+	semilogy(collect(1:length(ds_high)), ds_high, color=MBLUE, style,
+	         label=L"$ (r, p) = (5, 0.40) $")
+	r = 10; m = 8 * r * d
+	prob_mild = CompOpt.genSymQuadProb(d, m, r, 0.25)
+	prob_high = CompOpt.genSymQuadProb(d, m, r, 0.40)
+	println("Running for rank $(r)...")
+	if algo_type == :subgradient
+		_, ds_mild = CompOpt.pSgd_init(prob_mild, iters, delta, eps=1e-12)
+		_, ds_high = CompOpt.pSgd_init(prob_high, iters, delta, eps=1e-12)
+	else
+		_, ds_mild = CompOpt.proxlin_init(prob_mild, delta, iters, eps=1e-12)
+		_, ds_high = CompOpt.proxlin_init(prob_high, delta, iters, eps=1e-12)
+	end
+	semilogy(collect(1:length(ds_mild)), ds_mild, color=HBLUE, style,
+	         label=L"$ (r, p) = (10, 0.25) $")
+	semilogy(collect(1:length(ds_high)), ds_high, color="black", style,
+	         label=L"$ (r, p) = (10, 0.40) $")
+	xlabel(L"$ k $"); ylabel("Normalized error")
+	title("Quadratic sensing - $(algo_type) method"); legend(); show()
+end
+
+
+#= matrix completion =#
+function matcomp_experiment(d, r, iters, delta; algo_type=:subgradient)
+	pCol = [LBLUE, HBLUE, "black"]; idx = 0
+	for sample_freq in 0.1:0.05:0.2
+		println("Running for sample frequency $(sample_freq)")
+		idx += 1
+		prob = CompOpt.genMatCompProb(d, r, sample_freq)
+		if algo_type == :subgradient
+			_, ds = CompOpt.pSgd_init(prob, iters, delta)
+			# compare with gd
+			_, ds_grad = CompOpt.matCompNaiveGD_init(prob, delta, iters, 0.004)
+			zero_pad!(ds, iters); zero_pad!(ds_grad, iters)
+			semilogy(collect(1:iters), ds, color=pCol[idx],
+			         label=latexstring("p = $sample_freq"));
+			semilogy(collect(1:iters), ds_grad, color=pCol[idx], "--")
+		else
+			_, ds = CompOpt.matCompProxLinear_init(prob, iters, delta)
+			zero_pad!(ds, iters)
+			semilogy(collect(1:iters), ds, color=pCol[idx], ".-",
+			         label=latexstring("p = $sample_freq"))
+		end
+	end
+	xlabel(L"$ k $"); ylabel("Normalized error")
+	title("Matrix completion - $(algo_type) method vs. gradient descent")
+	legend(); show()
+end
+
+
+function main()
+	# parse arguments
+	s = ArgParseSettings(description="""
+						 Generates a set of synthetic problem instances for
+						 a given ratio m / dim and failure probabilities and
+						 solves them using a specified method.
+						 Outputs the convergence history in a .csv file.""")
+	@add_arg_table s begin
+		"--d1"
+			help = "Dimension 1 of the problem"
+			arg_type = Int
+			default = 200
+		"--d2"
+			help = "Dimension 2 of the problem (only for bilinear sensing)"
+			arg_type = Int
+			default = 200
+		"--r"
+			help = "The rank of the matrices involved"
+			arg_type = Int
+			default = 5
+		"--seed"
+			help = "The seed of the RNG"
+			arg_type = Int
+			default = 999
+		"--corr_lvl"
+			help = """
+			The level of corruption, which translates to fraction of corrupted
+			measurements in matrix sensing and robust pca."""
+			arg_type = Float64
+			default = 0.25
+		"--prob_type"
+			help =
+				"""
+				The type of the problem. `quadratic` results in a quadratic
+				problem with symmetrized measurements, and `bilinear` results
+				in a bilinear problem.
+				`matcomp` results in a matrix completion problem, while `rpca`
+				results in an instance of robust PCA."""
+			range_tester = (x -> lowercase(x) in [
+				"quadratic", "bilinear", "matcomp", "rpca"])
+			default = "bilinear"
+		"--algo_type"
+			help =
+				"""
+				The iterative algorithm to be used. `subgradient` denotes the
+				subgradient method with geometrically decaying step size or
+				the Polyak step size when the minimum value is known. `proxlinear`
+				denotes the prox-linear method, tailored to the specific problem
+				at hand."""
+			range_tester = (x -> lowercase(x) in [
+				"subgradient", "proxlinear"])
+			default = "subgradient"
+		"--iters"
+			help = "The number of iterations for minimization"
+			arg_type = Int
+			default = 1000
+		"--delta"
+			help = "The initial distance to ground truth"
+			arg_type = Float64
+			default = 0.95
+	end
+	parsed = parse_args(s)
+	d1, d2, r, rnd_seed = parsed["d1"], parsed["d2"], parsed["r"], parsed["seed"]
+	prob_type, iters, delta = parsed["prob_type"], parsed["iters"], parsed["delta"]
+	algo_type = parsed["algo_type"]
+	# seed RNG
+	Random.seed!(rnd_seed)
+	df = nothing;
+	if prob_type == "quadratic"
+		algo = (algo_type == "subgradient") ? :subgradient : :proxlinear
+		quad_experiment(d1, r, iters, delta, algo_type=algo)
+	elseif prob_type == "bilinear"
+		algo = (algo_type == "subgradient") ? :subgradient : :proxlinear
+		bilin_experiment(d1, d2, r, iters, delta, algo_type=algo)
+	elseif prob_type == "matcomp"
+		algo = (algo_type == "subgradient") ? :subgradient : :proxlinear
+		df = matcomp_experiment(d1, r, iters, delta, algo_type=algo)
+	else
+		throw(Exception("Not implemented yet"))
+	end
+end
+
+main()
---
+++ recovery_tolerance.jl
@@ -0,0 +1,94 @@
+using ArgParse
+using DataFrames
+using LinearAlgebra
+using Printf
+using PyPlot
+using Random
+using Statistics
+
+include("src/CompOpt.jl")
+
+
+zero_pad!(v, lenTotal) = append!(v, zeros(lenTotal - length(v)))
+
+#= symmetrized quadratic sensing =#
+function quad_dense(d, i, delta, dLvlList)
+	r = 5
+	for dLvl in dLvlList
+		figure()
+		# additive gaussian noise
+		prob = CompOpt.genSymQuadProb(d, i * r * d, r, 0.25, denseLvl=dLvl)
+		_, ds_sg = CompOpt.pSgd_init(prob, 500, delta, eps=1e-12)
+		_, ds_pl = CompOpt.symQuadProxlin_init(prob, delta, 20, eps=1e-12)
+		subplot(211); semilogy(collect(1:length(ds_sg)), ds_sg)
+		xlabel(L"$ k $"); ylabel("Normalized error")
+		title("Subgradient method")
+		subplot(212); semilogy(collect(1:length(ds_pl)), ds_pl)
+		xlabel(L"$ k $"); ylabel("Normalized error")
+		title("Prox-linear method")
+		show()
+	end
+end
+
+
+#= matrix completion =#
+function matcomp_dense(d, delta, dLvlList)
+	r = 8
+	for dLvl in dLvlList
+		figure()
+		prob = CompOpt.genMatCompProb(d, r, 0.25, denseLvl=dLvl)
+		_, ds_sg = CompOpt.pSgd_init(prob, 500, delta, eps=1e-12)
+		_, ds_pl = CompOpt.matCompProxLinear_init(prob, 20, delta)
+		subplot(211); semilogy(collect(1:length(ds_sg)), ds_sg)
+		xlabel(L"$k$"); ylabel(L"$ \\| X_k X_k^\\top - M_{\\sharp} \\|_F / \\| M_{\\sharp} \\|_F")
+		title("Subgradient method")
+		subplot(212); semilogy(collect(1:length(ds_pl)), ds_pl)
+		xlabel(L"$k$"); ylabel(L"$ \\| X_k X_k^\\top - M_{\\sharp} \\|_F / \\| M_{\\sharp} \\|_F")
+		title("Prox-linear method")
+		show()
+	end
+end
+
+
+function main()
+	# parse arguments
+	s = ArgParseSettings(description="""
+						 Solves symmetrized quadratic sensing and matrix
+						 completion problems with dense additive noise, plotting
+						 the convergence history.""")
+	@add_arg_table s begin
+		"--d"
+			help = "Dimension of the problem"
+			arg_type = Int
+			default = 200
+		"--seed"
+			help = "The seed of the RNG"
+			arg_type = Int
+			default = 999
+		"--denseLvl"
+			help = """
+			The levels of dense corruption, which translate to intensity of
+			additive Gaussian noise"""
+			arg_type = Float64
+			nargs = '*'
+		"--i"
+			help = "The ratio of measurements to the problem's dimension"
+			arg_type = Int
+			default = 8
+		"--delta"
+			help = "The initial distance to ground truth"
+			arg_type = Float64
+			default = 0.8
+	end
+	parsed = parse_args(s)
+	d, rnd_seed, i = parsed["d"], parsed["seed"], parsed["i"]
+	delta = parsed["delta"]; dLvlList = parsed["denseLvl"]; i = parsed["i"]
+	# seed RNG
+	Random.seed!(rnd_seed)
+	println("Running quadratic experiment...")
+	quad_dense(d, i, delta, dLvlList)
+	println("Running matcomp experiment...")
+	matcomp_dense(d, delta, dLvlList)
+end
+
+main()
